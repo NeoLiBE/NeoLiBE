3f7902c209bfe3afa3b71938b26ae0c021509c88
Neotropical Lizards Behavioral Ecology <galdinoc@gmail.com>
2014-07-01 20:47:00 -0300

    Update overlapHR.R

--- overlapHR.R
+++ overlapHR.R
@@ -1,4 +1,5 @@
 #######################################################################################################
+#######################################################################################################
 #
 # overlapHR
 # A R code to automate animals home range estimates and overlap based on minimum convex polygon method
@@ -9,7 +10,7 @@
 #######################################################################################################
 # These lines of code are product of the Neotropical Lizard Behavioral Ecology-NeoLiBE group from Brazil
 #
-# By Daniel C. Passos and Conrado A. B. Galdino 
+# By Daniel C. Passos and Conrado A. B. Galdino
 # Version v1.0rc1; October 2013
 # Reviewed: 07 May 2014
 # Last Review: 01 July 2014
@@ -17,14 +18,14 @@
 #########################################################################################################
 
 #########################################################################################################################
-# ATTENTION: You are free to use, modify and forward this code under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 :)  ########################################################################################################################
+# ATTENTION: You are free to use, modify and forward this code under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 :) ########################################################################################################################
 
 #######################################################################################################
-#  ACKNOWLEDGMENTS:
+# ACKNOWLEDGMENTS:
 #
-#  We are grateful to Augusto Ribas for providing the code on HR overlap http://r-sig-ecology.471788.n2.nabble.com/Estimating-HR-overlap-by-adehabitatHR-td7409589.html
+# We are grateful to Augusto Ribas for providing the code on HR overlap http://r-sig-ecology.471788.n2.nabble.com/Estimating-HR-overlap-by-adehabitatHR-td7409589.html
 #
-#  CABG Thanks Fundação de Amparo à Pesquisa do Estado de Minas Gerais-FAPEMIG for the support
+# CABG Thanks Fundação de Amparo à Pesquisa do Estado de Minas Gerais-FAPEMIG for the support
 #
 #########################################################################################################
 
@@ -37,11 +38,11 @@
 #############################
 
 print("If you need to cite this code please use: Passos, D. C; Galdino C. A. B; Rocha, C. F. D. 20XX. Challenges and perspectives for studies on home range of Brazilian lizards. South American Jour. Herpetol. xx:yy-zz")
-cat("Just press [enter] again")
+cat("Just press [enter]")
 readline();
 print("&")
 print(citation("adehabitatHR"))
-cat ("Just press [enter] again")
+cat ("Just press [enter] again!")
 readline();
 
 
@@ -50,24 +51,27 @@ readline();
 ###################################################
 
 if(is.element('rgeos', installed.packages()[,1]) == FALSE) {
-	install.packages('rgeos') 
+install.packages('rgeos')
 }else(is.element('rgeos', installed.packages()[,1]) == TRUE)
 {print("You already have installed the rgeos package!")
 }
 #Check if user want to load package rgeos
-repeat 
+repeat
 {
 print("Do you want to load rgeos at this time?[y] or [n]")
 a=scan(what=character(),nmax=1);
 
 if(a=="y"){
-	print("Loading the package")
-	library('rgeos')
-	print("rgeos package is ready to use")
-	print("next time you need to load rgeos package please do it manually");
+print("Loading the package")
+library('rgeos')
+print("rgeos package is ready to use")
+writeLines("")
+print("next time you need to load rgeos package please do it manually");
 break;
 }else if (a=="n"){
-	print("Ok, maybe you consider to do it later")
+writeLines("")
+print("Ok, maybe you consider to do it later")
+writeLines("")
 break;
 
 }else {print("Please type lower case y or n")
@@ -80,7 +84,7 @@ break;
 ##########################################################
 
 if(is.element('adehabitatHR', installed.packages()[,1]) == FALSE) {
-	install.packages('adehabitatHR') 
+install.packages('adehabitatHR')
 }else {is.element('adehabitatHR', installed.packages()[,1]) == TRUE}
 {print("You already have installed the adehabitatHR package!")
 }
@@ -90,28 +94,29 @@ repeat
 {
 print("Do you want to load adehabitatHR at this time?[y] or [n]")
 cat(" "); b=scan(what=character(),nmax=1);
-if(b=="y"){ 
-	print("Loading adehabitatHR package")
-	library('adehabitatHR')
-	cat ("Just press [enter] again")
-	readline();
-	print("adehabitatHR is ready to use")
-	print("next time you need to load adehabitatHR please do it manually")
+if(b=="y"){
+print("Loading adehabitatHR package")
+library('adehabitatHR')
+cat ("Just press [enter] again")
+readline();
+print("adehabitatHR is ready to use")
+print("next time you need to load adehabitatHR please do it manually")
 break;
 }else if (b=="n"){
-	print("Ok, maybe you consider to do it later")
+print("Ok, maybe you consider to do it later")
+writeLines("")
 break;
 }else {print("Please type lower case y or n")
 }
 }
 
 ######################
-#Loading data into R 
-#ATTENTION: We are assuming that the data file (here named spatdata.csv) have a colection of  x and y points for each individual; we recommend you to see and run this code with the the example file spatdata.csv"
+#Loading data into R
+#ATTENTION: We are assuming that the data file (here named spatdata.csv) have a colection of x and y points for each individual; we recommend you to see and run this code with the the example file spatdata.csv"
 # ATTENTION: The package adehabitatHR will not generate polygons if you have less than 5 points per individual
 ######################
 
-pointdata = read.csv("spatdata.csv", header=TRUE, sep=",")  # pay spetial attention in the type of columm separator you're using here we used "," you can change it accordling.
+pointdata = read.csv("spatdata.csv", header=TRUE, sep=",") # pay spetial attention in the type of columm separator you're using here we used "," you can change it accordling.
 
 #Check if user want to see the object with stored data.
 print("Do you want to inspect your R object corresponding to your data file? [y] or [n]")
@@ -120,11 +125,11 @@ print("Press [enter], please!")
 cat(" "); g=scan(what=character(),nmax=1);
 repeat
 {
-if(g=="y"){ 
-	print(pointdata)	
+if(g=="y"){
+print(pointdata)	
 break;
-}else if(c=="n"){
-	print("Ok, maybe later!")
+}else if(g=="n"){
+print("Ok, maybe later!")
 break;
 }else{ print("Please type lower case y or n")
 }
@@ -145,7 +150,7 @@ coordinates(identities)<-locations
 #Generating home ranges R object for each individual in a sample and asking for show and write a file with HR data#
 ###################################################################################################################
 
-hranges=mcp(identities, percent=95, unin='m', unout='m2')    # Generates the object “hranges” that contains the home range estimates in squared meters for each individual.
+hranges=mcp(identities, percent=95, unin='m', unout='m2') # Generates the object “hranges” that contains the home range estimates in squared meters for each individual.
 
 hranges.df=as.data.frame(hranges)
 
@@ -156,13 +161,13 @@ print("Press [enter], please!")
 cat(" "); c=scan(what=character(),nmax=1);
 repeat
 {
-if(c=="y"){ 
-	print(hranges.df)	
-	print("Check in your work folder for HRanges.csv file")
-	write.csv(hranges.df, "HRanges.csv")
+if(c=="y"){
+print(hranges.df)	
+print("Check in your work folder for HRanges.csv file")
+write.csv(hranges.df, "HRanges.csv")
 break;
 }else if(c=="n"){
-	print("Ok, maybe you consider to do it later")
+print("Ok, maybe you consider to do it later")
 break;
 }else{ print("Please type lower case y or n")
 }
@@ -177,17 +182,17 @@ print("Press [enter], please!")
 cat(" "); c=scan(what=character(),nmax=1);
 repeat
 {
-if(c=="y"){ 
-	plot(hranges)
+if(c=="y"){
+plot(hranges)
 break;
 }else if(c=="n"){
-	print("Ok, maybe you consider to do it later")
+print("Ok, maybe you consider to do it later")
 break;
 }else{ print("Please type lower case y or n")
 }
 }
 ###########################################################################################################
-#Generating the matrix with the ABSOLUTE values of pairs of individuals ranges overlaps 
+#Generating the matrix with the ABSOLUTE values of pairs of individuals ranges overlaps
 ###########################################################################################################
 print("Generating matrix with ABSOLUTE overlap values")
 cat ("Press [enter] please")
@@ -211,14 +216,15 @@ write.csv(abs_Matrix, "The_Matrix.csv")
 
 
 ###########################################################################################################
-#Generating the matrix with the RELATIVE values of pairs of individuals ranges overlaps 
+#Generating the matrix with the RELATIVE values of pairs of individuals ranges overlaps
 ###########################################################################################################
-
+writeLines("")
+writeLines("")
 print("Generating matrix with relative overlap values")
 cat ("Press [enter] please")
 readline();
 rel_Matrix <- matrix(NA, abs(length(levels(pointdata$id))), abs(length(levels(pointdata$id))), dimnames=list(c(levels(pointdata$id)),c(levels(pointdata$id))))
-	for(i in 1:abs(length(levels(pointdata$id)))){
+for(i in 1:abs(length(levels(pointdata$id)))){
                areas.percent<-(abs_Matrix[,i])/hranges$area[i]
                rel_Matrix[,i]<-areas.percent
                }
@@ -229,3 +235,7 @@ readline();
 print("Check in your work folder for Rel_Matrix.csv file")
 write.csv(rel_Matrix, "rel_Matrix.csv")
 
+writeLines("")
+writeLines("")
+print("Hope now you have all results needed")
+print("end [fim] of this routine")
